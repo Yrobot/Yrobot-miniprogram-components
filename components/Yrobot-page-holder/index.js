bfcc3115baa8ab5260c2fd3989ac8c3aba897c21
Yrobot <Yrobot@qq.com>
2018-12-22 11:44:09 +0800

    实现Yrobot-page-holder

--- components/Yrobot-page-holder/index.js
+++ components/Yrobot-page-holder/index.js
@@ -1,21 +1,24 @@
-// Yrobot-layout/Yrobot-layout.js
-// Yrobot设计实现layout组件，作为整个页面的layout布局
+// Yrobot-pageHolde/Yrobot-pageHolde.js
+// Yrobot设计实现pageHolde组件，来hold页面 内容部分 和 遮蔽部分
 // 接入点主要包括 遮蔽层 和 内容层 
 // 时间：2018年12月22日 11:08:32
 
 Component({
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
   /**
    * 组件的属性列表
    */
   properties: {
-    
+
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    animationData: {},
+
   },
 
   /**
@@ -24,6 +27,6 @@ Component({
   ready() {
   },
   methods: {
-    
+
   }
 })
